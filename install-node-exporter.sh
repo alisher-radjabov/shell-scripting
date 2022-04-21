ed22a5aa49a7252be6e63dfe7ffba24897830797
Alisher Radjabov <advaster@yahoo.com>
2022-04-21 18:07:55 +0500

    Add files via upload

--- install-node-exporter.sh
+++ install-node-exporter.sh
@@ -1,10 +1,10 @@
-#!bin/bash
+#!/bin/bash
 wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 tar -xvf node_exporter-1.3.1.linux-amd64.tar.gz
 sudo mv -v node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/
 sudo chown root:root /usr/local/bin/node_exporter
 node_exporter --version
-echo '[Unit]
+sudo echo '[Unit]
 Description=Prometheus exporter for machine metrics
 
 [Service]
@@ -20,3 +20,4 @@ WantedBy=multi-user.target' > /etc/systemd/system/node-exporter.service
 sudo systemctl daemon-reload
 sudo systemctl start node-exporter.service
 sudo systemctl enable node-exporter.service
+sudo systemctl status node-exporter.service
